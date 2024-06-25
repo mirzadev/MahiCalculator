3d5751886e71a65a64c023644b5e14aa279998fc
Mirza Abdul Awal <84652300+mirzadev@users.noreply.github.com>
2024-06-25 14:44:08 -0400

    Clear button added

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ const AddBtn = document.getElementById("Addition") as HTMLButtonElement;
 const SubtBtn = document.getElementById("Subtraction") as HTMLButtonElement;
 const MultBtn = document.getElementById("Multiplication") as HTMLButtonElement;
 const DvnBtn = document.getElementById("Division") as HTMLButtonElement;
-
+const ClrBtn = document.getElementById("Clear") as HTMLButtonElement;
 const calResult = document.getElementById("result") as HTMLOutputElement;
 
 function checkNumberInRange(number: string): boolean {
@@ -83,3 +83,9 @@ function DivideNumbers() {
   calResult.textContent = result.toString();
 }
 DvnBtn.addEventListener("click", DivideNumbers);
+
+function clear() {
+  firstNum.value = "";
+  secondNum.value = "";
+}
+ClrBtn.addEventListener("click", clear);
